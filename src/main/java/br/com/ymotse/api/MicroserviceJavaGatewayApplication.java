12a552976b003e3f071a70fba01e0e52e38192f5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-03-06 19:56:03 -0300

    Share project.

---
+++ src/main/java/br/com/ymotse/api/MicroserviceJavaGatewayApplication.java
@@ -0,0 +1,26 @@
+package br.com.ymotse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 
+ * @author yitshhaq.fukushima
+ *
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication
+public class MicroserviceJavaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJavaGatewayApplication.class, args);
+	}
+
+}
