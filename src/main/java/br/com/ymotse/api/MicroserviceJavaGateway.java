7284b7af7188ec8ed6566946f1bdc78f5de9dde6
Yitshhaq Fukushima <ymotse@gmail.com>
2020-03-26 19:50:38 -0300

    Refactoring routing with Zuul.

--- src/main/java/br/com/ymotse/api/MicroserviceJavaGateway.java
+++ src/main/java/br/com/ymotse/api/MicroserviceJavaGateway.java
@@ -6,12 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import br.com.ymotse.api.filters.ErrorFilter;
-import br.com.ymotse.api.filters.PostFilter;
-import br.com.ymotse.api.filters.PreFilter;
-import br.com.ymotse.api.filters.RouterFilter;
 
 /**
  * 
@@ -23,31 +17,10 @@ import br.com.ymotse.api.filters.RouterFilter;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
-public class MicroserviceJavaGatewayApplication {
+public class MicroserviceJavaGateway {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceJavaGatewayApplication.class, args);
+		SpringApplication.run(MicroserviceJavaGateway.class, args);
 	}
-	
-	
-	@Bean
-	public ErrorFilter errorFilter() {
-		return errorFilter();
-	}
-	
-	@Bean
-	public PreFilter preFilter() {
-		return preFilter();
-	}
-	
-	@Bean
-	public PostFilter postFilter() {
-		return postFilter();
-	}
-	
-	@Bean
-	public RouterFilter routerFilter() {
-		return routerFilter();
-	}
-	
+
 }
